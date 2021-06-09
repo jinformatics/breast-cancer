6f21baf5ef21fd411357b02baabaa6ce373660b6
jinformatics <78642915+jinformatics@users.noreply.github.com>
2021-06-09 13:32:23 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -2,6 +2,10 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 import math
+import os
+
+port = int(os.environ.get('PORT' ,5000))
+app.run(host = '0.0.0.0' , port = port , debug = True)
 
 app = Flask(__name__)
 model = pickle.load(open('breast-cancer.pickle','rb'))
